b6d08cd62381d47eda20a86772fe8885c7a500b3
Jérôme Faucher <62873431+JeromeF99@users.noreply.github.com>
2021-06-26 18:14:06 +0200

    remove count

--- DeployWebsite.tf
+++ DeployWebsite.tf
@@ -67,14 +67,12 @@ data "aws_subnet" "subnet-public-1" {
 }
 
 data "aws_subnet" "subnet-public-2" {
-  count = var.env == "prod" ? 1 : 0
   tags = {
     Name = "${var.env}-subnet-public-2"
   }
 }
 
 data "aws_subnet" "subnet-public-3" {
-  count = var.env == "prod" ? 1 : 0
   tags = {
     Name = "${var.env}-subnet-public-3"
   }
@@ -87,14 +85,12 @@ data "aws_subnet" "subnet-private-1" {
 }
 
 data "aws_subnet" "subnet-private-2" {
-  count = var.env == "prod" ? 1 : 0
   tags = {
     Name = "${var.env}-subnet-private-2"
   }
 }
 
 data "aws_subnet" "subnet-private-3" {
-  count = var.env == "prod" ? 1 : 0
   tags = {
     Name = "${var.env}-subnet-private-3"
   }
